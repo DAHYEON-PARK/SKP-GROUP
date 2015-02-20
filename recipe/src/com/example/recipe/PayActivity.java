5a1416aa8fa397e1e9709a4d1d1184c9a30ebba4
sally <luckyclover0121@gmail.com>
2015-02-20 18:57:29 +0900

    버튼 클릭하면 payment 페이지로 이동

--- recipe/src/com/example/recipe/PayActivity.java
+++ recipe/src/com/example/recipe/PayActivity.java
@@ -1,7 +1,16 @@
 package com.example.recipe;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class PayActivity extends Activity{
 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.payment_page);
+	}
+	
+	
 }
